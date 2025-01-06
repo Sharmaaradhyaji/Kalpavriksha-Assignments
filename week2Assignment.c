2340bacc3bd216aeef9cc1c94a897f7b8cd2337d
aradhya sharmma <as5909906@gmail.com>
2025-01-06 12:42:25 +0530

    Updated week2Assignment.c

--- week2Assignment.c
+++ week2Assignment.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-long long evaluate(long long base, long long int powerOfBase, long long modulus){
-    if (powerOfBase==0) 
-        return 1%modulus; 
-    long long answer=evaluate(base,powerOfBase/2,modulus);  
-    answer=(answer*answer)%modulus;
-    if(powerOfBase%2==1) 
-        answer=(answer*base)%modulus;
+long long evaluate(long long base, long long int powerOfBase, long long modulus)
+{
+    if (powerOfBase == 0)
+        return 1 % modulus;
+    long long answer = evaluate(base, powerOfBase / 2, modulus);
+    answer = (answer * answer) % modulus;
+    if (powerOfBase % 2 == 1)
+        answer = (answer * base) % modulus;
     return answer;
 }
 
-int main(){
+int main()
+{
     long long base, powerOfBase, modulus;
     printf("Base: ");
     scanf("%lld", &base);
@@ -18,14 +20,15 @@ int main(){
     scanf("%lld", &powerOfBase);
     printf("Modulus: ");
     scanf("%lld", &modulus);
-    if(modulus<=1){
+    if (modulus <= 1)
+    {
         printf("Invalid input: Modulus must be > 1\n");
         return 1;
     }
-    if(base<=0){
+    if (base <= 0)
+    {
         printf("Invalid input: Base must be > 1\n");
     }
     printf("Result: %lld\n", evaluate(base, powerOfBase, modulus));
     return 0;
 }
-
