ae7422721b5cd1df9036851e17137d6f3e3b3302
aradhya sharmma <as5909906@gmail.com>
2025-02-18 15:46:32 +0530

    optimized code for anagram using LL and Hash

--- anagramLL.c
+++ anagramLL.c
@@ -1,56 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct Node
 {
-    char string[100];
+    char Word[100];
     struct Node *next;
 };
 
-struct Hash
+bool checkHash(int hash[128], char *word)
 {
-    char shabd[50];
-    struct Node *head;
-};
+    int hash1[128] = {0};
 
-struct Hash *List[100];
-
-void sort(char word[50])
-{
-    int len = strlen(word);
-    for (int i = 0; i < len - 1; i++)
+    int k = 0;
+    while (word[k] != '\0')
     {
-        for (int j = i + 1; j < len; j++)
+        hash1[word[k]]++;
+        k++;
+    }
+
+    for (int i = 0; i < 128; i++)
+    {
+        if (hash[i] != hash1[i])
         {
-            if (word[i] > word[j])
-            {
-                char temp = word[i];
-                word[i] = word[j];
-                word[j] = temp;
-            }
+            return false;
         }
+    }
+    return true;
+}
+
+void printGroup(struct Node *head, int group)
+{
+    printf("Word Group %d: ", group);
+    struct Node *temp = head;
+    while (temp)
+    {
+        printf("%s -> ", temp->Word);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node *head)
+{
+    struct Node *temp;
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
     }
 }
 
 int main()
 {
-    char *words[] = {"listen", "silent", "enlist", "rat", "tar", "art", "dog", "god"};
+    char *words[] = {"listen", "dog", "art", "rat", "tar", "enlist", "silent", "god"};
     int n = 8;
-
-    for (int i = 0; i < n; i++)
-    {
-        List[i] = (struct Hash *)malloc(sizeof(struct Hash));
-        List[i]->head = NULL;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        char sortedString[50];
-        strcpy(sortedString, words[i]);
-        sort(sortedString);
-        strcpy(List[i]->shabd, sortedString);
-    }
 
     int processed[n];
     for (int i = 0; i < n; i++)
@@ -58,89 +64,53 @@ int main()
         processed[i] = 0;
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        if (processed[i])
-            continue;
-
-        char word[50];
-        strcpy(word, List[i]->shabd);
-
-        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-        strcpy(newNode->string, words[i]);
-        newNode->next = NULL;
-
-        if (List[i]->head == NULL)
-        {
-            List[i]->head = newNode;
-        }
-        else
-        {
-            struct Node *temp = List[i]->head;
-            while (temp->next)
-                temp = temp->next;
-            temp->next = newNode;
-        }
-
-        for (int j = i + 1; j < n; j++)
-        {
-            if (processed[j])
-                continue;
-            if (strcmp(word, List[j]->shabd) == 0)
-            {
-                struct Node *tempNode = (struct Node *)malloc(sizeof(struct Node));
-                strcpy(tempNode->string, words[j]);
-                tempNode->next = NULL;
-
-                struct Node *temp = List[i]->head;
-                while (temp->next)
-                    temp = temp->next;
-                temp->next = tempNode;
-
-                processed[j] = 1;
-            }
-        }
-        processed[i] = 1;
-    }
-
     int group = 1;
     for (int i = 0; i < n; i++)
     {
-        if (List[i] && List[i]->head)
+        int hash[128] = {0};
+
+        int k = 0;
+        while (words[i][k] != '\0')
         {
-            struct Node *temp = List[i]->head;
-            printf("Anagram Group %d: ", group++);
-            while (temp)
+            hash[words[i][k]]++;
+            k++;
+        }
+
+        struct Node *head = NULL;
+        if (processed[i])
+        {
+            continue;
+        }
+        for (int j = i; j < n; j++)
+        {
+            if (processed[j])
             {
-                printf("%s -> ", temp->string);
-                temp = temp->next;
+                continue;
             }
-            printf("NULL\n");
-            for (int j = i + 1; j < n; j++)
+            if (checkHash(hash, words[j]))
             {
-                if (strcmp(List[i]->shabd, List[j]->shabd) == 0)
+                processed[j] = 1;
+                struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+                strcpy(newNode->Word, words[j]);
+                newNode->next = NULL;
+                if (!head)
                 {
-                    List[j] = NULL;
+                    head = newNode;
+                }
+                else
+                {
+                    struct Node *temp = head;
+                    while (temp->next)
+                    {
+                        temp = temp->next;
+                    }
+                    temp->next = newNode;
                 }
             }
         }
+        printGroup(head, group++);
+        freeList(head);
     }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (List[i])
-        {
-            struct Node *temp = List[i]->head;
-            while (temp)
-            {
-                struct Node *toDelete = temp;
-                temp = temp->next;
-                free(toDelete);
-            }
-            free(List[i]);
-        }
-    }
-
     return 0;
 }
 
