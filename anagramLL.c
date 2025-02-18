dabd495055013432e24849a5bca8f0a80eead48e
aradhya sharmma <as5909906@gmail.com>
2025-02-18 15:43:32 +0530

    optimized code for anagral using LL and Hash

---
+++ anagramLL.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct Node
+{
+    char Word[100];
+    struct Node *next;
+};
+
+bool checkHash(int hash[128], char *word)
+{
+    int hash1[128] = {0};
+
+    int k = 0;
+    while (word[k] != '\0')
+    {
+        hash1[word[k]]++;
+        k++;
+    }
+
+    for (int i = 0; i < 128; i++)
+    {
+        if (hash[i] != hash1[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printGroup(struct Node *head, int group)
+{
+    printf("Word Group %d: ", group);
+    struct Node *temp = head;
+    while (temp)
+    {
+        printf("%s -> ", temp->Word);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node *head)
+{
+    struct Node *temp;
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main()
+{
+    char *words[] = {"listen", "silent", "art", "rat", "tar", "enlist", "dog", "god"};
+    int n = 8;
+
+    int processed[n];
+    for (int i = 0; i < n; i++)
+    {
+        processed[i] = 0;
+    }
+
+    int group = 1;
+    for (int i = 0; i < n; i++)
+    {
+        int hash[128] = {0};
+
+        int k = 0;
+        while (words[i][k] != '\0')
+        {
+            hash[words[i][k]]++;
+            k++;
+        }
+
+        struct Node *head = NULL;
+        if (processed[i])
+        {
+            continue;
+        }
+        for (int j = i; j < n; j++)
+        {
+            if (processed[j])
+            {
+                continue;
+            }
+            if (checkHash(hash, words[j]))
+            {
+                processed[j] = 1;
+                struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+                strcpy(newNode->Word, words[j]);
+                newNode->next = NULL;
+                if (!head)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    struct Node *temp = head;
+                    while (temp->next)
+                    {
+                        temp = temp->next;
+                    }
+                    temp->next = newNode;
+                }
+            }
+        }
+        printGroup(head, group++);
+        freeList(head);
+    }
+    return 0;
+}
+
